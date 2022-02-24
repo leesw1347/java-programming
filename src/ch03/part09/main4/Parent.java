a3c63cc65941535baf2884ffe01ee8ba703ffd57
leesw1347 <leesw1347@gmail.com>
2022-02-25 00:35:29 +0900

    이전 작업 메세지

--- src/ch03/part09/main4/Parent.java
+++ src/ch03/part09/main4/Parent.java
@@ -1,11 +1,11 @@
 package ch03.part09.main4;
 
 public class Parent {
-    public void testMethod1(){
+    public void testMethod1() {
         System.out.println("testMethod1 함수호출");
     }
 
-    public void testMethod2(){
+    public void testMethod2() {
         System.out.println("testMethod2 함수 호출");
     }
 
