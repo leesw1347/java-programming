b557b32f5368963a949a957f7426f1c83caff1be
gigkgka <leesw1347@gmail.com>
2022-03-16 08:55:48 +0900

    추상 클래스를 이용한 모듈화

--- src/ch09/part03/main5/sub2/ReadFileImpl.java
+++ src/ch09/part03/main5/sub2/ReadFileImpl.java
@@ -1,5 +1,13 @@
 package ch09.part03.main5.sub2;
 
+/**
+ * 클래스 모듈화
+ * [1]. 추상 클래스를 이용한 모듈화
+ *  클래스로 구성이 되어 하나의 타입으로 사용된다
+ *  해당 타입으로 객체 생성 후 파라미터로 전달이 가능하다.
+ *  업무 절차를 갖는 프레임 구성에 있어서 매우 편리하다
+ *  업무 절차를 구성 후 해당 가이드대로 로직만 구성하면 된다.
+ */
 public class ReadFileImpl extends AbstractReadFile {
     // 파일 경로를 설정하기위한 getFilePath() 함수 재정의
     @Override
