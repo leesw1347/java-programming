04ec55d83b95b927d64fa65f93c611f4143013cb
gigkgka <leesw1347@gmail.com>
2022-03-19 06:21:07 +0900

    정적(static) 전역변수의 자료접근

--- src/ch11/part01/main6/TestMain.java
+++ src/ch11/part01/main6/TestMain.java
@@ -7,36 +7,33 @@ public class TestMain {
     private static String name2 = "TestMain 정적 전역변수 name2";
 
     public void method() {
-        InnerClass c = new InnerClass() {
-            @Override
-            public void method2() {
-                System.out.println("InnerClass method2() 재정의");
-
-                // 문제1. TestMain 클래스 전역변수 name
-                System.out.println(TestMain.name);
-
-                // 문제2. InnerClass 클래스 전역변수 name
-                System.out.println(InnerClass.name);
-
-                // 문제3. InnerClass 클래스 지역변수 name
-                System.out.println(InnerClass.name);
-
-                // 문제4. TestMain 클래스 전역변수 name2
-                System.out.println(TestMain.name2);
-
-                // InnerClass 클래스 전역변수 name2
-//                System.out.println(InnerClass.name2);
-
-            }
-        };
+        InnerClass c = new InnerClass();
         c.method2();
     }
 
-    public abstract static class InnerClass {
+    public static class InnerClass {
         private static String name = "InnerClass 정적 전역변수 name";
         private String name2 = "InnerClass 전역변수 name2";
 
-        public abstract void method2();
+        public void method2() {
+            String name = "InnerClass 지역변수 name";
+
+            // 다음의 변수를 구분하여 콘솔화면에 출력하시오
+            // 문제1. TestMain 클래스 전역변수 name
+            System.out.println(TestMain.name);
+
+            // 문제2. InnerClass 클래스 전역변수 name
+            System.out.println(TestMain.name2);
+
+            // 문제3. InnerClass 클래스 지역변수 name
+            System.out.println(InnerClass.name);
+
+            // 문제4. TestMain 클래스 전역변수 name2
+            System.out.println(TestMain.name2);
+
+            // 문제5. InnerClass 클래스 전역변수 name2
+            System.out.println(name);
+        }
     }
 
     public static void main(String[] args) {
