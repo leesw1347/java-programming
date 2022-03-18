af9bd3d013f4ce4e54627559380a2ea75ce0dfed
gigkgka <leesw1347@gmail.com>
2022-03-19 07:13:39 +0900

    로직 내부에서 블록과 블록 사이의 자료 연결

--- src/ch11/part02/main1/TestMain.java
+++ src/ch11/part02/main1/TestMain.java
@@ -1,5 +1,21 @@
 package ch11.part02.main1;
 
-
 public class TestMain {
+    public static void main(String[] args) {
+        // 블록1과 블록2에 자료연결을 위해 블록1의 상위블록에 정의
+        String name = null;
+
+        /**
+         * 블록1
+         */
+        {
+            name = "블록1 name";
+        }
+        /**
+         * 블록2
+         */
+        {
+            // System.out.println(name); // 생성된 name 변수를 출력
+        }
+    }
 }
