4c7e2920d960759130812b049ecec1ce57ae1800
gigkgka <leesw1347@gmail.com>
2022-03-19 07:23:16 +0900

    로직 내부에서 지역변수와 익명 클래스 함수 내부의 자료 연결

--- src/ch11/part02/main2/TestMain.java
+++ src/ch11/part02/main2/TestMain.java
@@ -1,4 +1,21 @@
 package ch11.part02.main2;
 
 public class TestMain {
+    public static void main(String[] args) {
+        // name 전역변수 정의
+        String name = "지역변수 name";
+        // ITest 익명클래스 객체생성
+        ITest test = new ITest() {
+            @Override
+            public void method() {
+                // 지역변수에서 익명클래스 내부함수로의 자료전달
+                System.out.println(name); // final 키워드가 생략되어 있으므로, name의 값 변경이 불가능하다
+            }
+        };
+        test.method();
+    }
+
+    public interface ITest {
+        public void method();
+    }
 }
