b4da87d05b771552123fc43ea6ea943f8c1ceca3
gigkgka <leesw1347@gmail.com>
2022-03-19 07:39:23 +0900

    클래스 내 자료 연결

--- src/ch11/part02/main3/TestMain.java
+++ src/ch11/part02/main3/TestMain.java
@@ -4,11 +4,21 @@ package ch11.part02.main3;
 public class TestMain {
     // setName()으로 들어오는 파라미터 name의 값을 print() 함수 실행 시
     // 콘솔 화면에 변수 name의 값이 출력된다.
-    public void setName(String name){
+    // 전역변수 name을 정의하여 함수와 함수 사이의 자료를 연결한다
+    private String name; //  = "TestMain 전역변수 정의";
 
+    public void setName(String name) {
+        /**
+         * 외부에서 들어오는 파라미터를 전역변수로 전달한다
+         * setName() 함수 내부 : 지역변수 name, 전역변수 name에 자료 전달
+         */
+        this.name = name;
     }
 
-    public void print(){
+    /**
+     * 전역변수 name 자료 접근
+     */
+    public void print() {
         // setName() 으로 들어오는 name 값을 출력하시오
         System.out.println(name);
     }
