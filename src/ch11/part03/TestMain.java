8d47f2c78b5b15544dd54b26cefc889fb9fff1aa
gigkgka <leesw1347@gmail.com>
2022-03-20 19:31:37 +0900

    공통 로직의 처리

---
+++ src/ch11/part03/TestMain.java
@@ -0,0 +1,11 @@
+package ch11.part03;
+
+public class TestMain {
+    public void read() throws Exception {
+        // 1) 파일의 경로(path)를 지정
+        String path = "src/ch11/part03/main1/TestMain.java";
+
+        // 절차1. 분리구간 시작
+
+    }
+}
