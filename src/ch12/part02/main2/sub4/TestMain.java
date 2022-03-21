d52cd57ed96e73832d0fc064abc3dd180e39baa9
gigkgka <leesw1347@gmail.com>
2022-03-21 15:59:43 +0900

    기타 기능 사용 예제 - 자료추출 및 일괄처리

---
+++ src/ch12/part02/main2/sub4/TestMain.java
@@ -0,0 +1,21 @@
+package ch12.part02.main2.sub4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestMain {
+    public static void main(String[] args) {
+        // List 객체생성 및 자료등록
+        List<String> list1 = new ArrayList<>();
+        list1.add("a");
+        list1.add("b");
+        list1.add("c");
+        list1.add("d");
+
+        System.out.println("[List1]" + list1);
+        System.out.println();
+
+
+
+    }
+}
