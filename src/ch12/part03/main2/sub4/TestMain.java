edf6988c901c4987b7e81b11d5323c1134595d91
gigkgka <leesw1347@gmail.com>
2022-03-24 08:40:49 +0900

    기타 기능 사용 예제 - 자료 추출 및 일괄처리

--- src/ch12/part03/main2/sub4/TestMain.java
+++ src/ch12/part03/main2/sub4/TestMain.java
@@ -43,6 +43,29 @@ public class TestMain {
         System.out.println("3. 일괄자료포함여부 --");
         System.out.println(MessageFormat.format("\t[list3] {0}", list3));
         boolean containsAll1 = list1.containsAll(list3);
+        System.out.println(MessageFormat.format("\t list1.containsAll(list3) = {0}", containsAll1));
+        System.out.println("[list1] " + list1);
         System.out.println();
+
+        // 4. 일괄자료삭제
+        List<String> list4 = new ArrayList<>();
+        list4.add("a");
+        list4.add("b");
+        list4.add("c");
+        list1.removeAll(list4); // list1에 있는 값을 list4에 해당하는 값들을 제거한다
+        System.out.println("4. 일괄자료삭제 --");
+        System.out.println("\t[list4] " + list4);
+        System.out.println(MessageFormat.format("[list1]list1.removeAll(list4){0}", list1));
+        System.out.println();
+
+        // 5. 공통자료만 남기기
+        List<String> list5 = new ArrayList<>();
+        list5.add("d");
+        list5.add("e");
+        list5.add("c");
+        list1.retainAll(list5);
+        System.out.println("5. 공통자료 남기기 --");
+        System.out.println(MessageFormat.format("\t[list5] {0}", list5));
+        System.out.println(MessageFormat.format("[list1]list1.retainAll(list5) = {0}", list1));
     }
 }
