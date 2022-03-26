6cf5dd9ebd614fb92b31f73c5eb4c8a09001eafc
gigkgka <leesw1347@gmail.com>
2022-03-26 16:47:48 +0900

    기본 기능 (CRUD) 사용 예제 [1]

---
+++ src/ch12/part04/main2/sub2/TestMain.java
@@ -0,0 +1,12 @@
+package ch12.part04.main2.sub2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMain {
+    public static void main(String[] args) {
+        // 객체생성
+        Map<String, Integer> map1 = new HashMap<>();
+        
+    }
+}
