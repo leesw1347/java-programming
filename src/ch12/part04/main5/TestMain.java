3270964f54d70158caeafd27284565356b1abb36
gigkgka <leesw1347@gmail.com>
2022-03-26 22:33:45 +0900

    주의사항 - null 자료의 처리

---
+++ src/ch12/part04/main5/TestMain.java
@@ -0,0 +1,18 @@
+package ch12.part04.main5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TestMain {
+    public static void main(String[] args) {
+
+        // 객체생성
+        System.out.println(" 객체생성 및 자료등록 ");
+        Map<String, String> map1 = new HashMap<>();
+        map1.put("1", "a");
+        map1.put("2", "b");
+        map1.put(null, "e");
+        map1.put()
+
+    }
+}
