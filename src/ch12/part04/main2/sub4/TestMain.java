8ada9d507aadc77221ddb03ab619db2fbfac43d3
gigkgka <leesw1347@gmail.com>
2022-03-26 21:43:17 +0900

    기타 기능 사용 예제 - 일괄 처리 및 전체 자료 조회

--- src/ch12/part04/main2/sub4/TestMain.java
+++ src/ch12/part04/main2/sub4/TestMain.java
@@ -1,7 +1,7 @@
 package ch12.part04.main2.sub4;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.nio.file.FileSystemNotFoundException;
+import java.util.*;
 
 public class TestMain {
     public static void main(String[] args) {
@@ -16,6 +16,33 @@ public class TestMain {
         System.out.println("map1 = " + map1);
         System.out.println();
 
+        // 2. 일괄자료등록
+        System.out.println(" 자료일괄등록 - map1.putAll(map2)");
+        Map<String, String> map2 = new HashMap<>();
+        map2.put("1", "e");
+        map2.put("2", "f");
+        map2.put("5", "g");
+        map2.put("6", "h");
+        map1.putAll(map2);
+        System.out.println("map2 = " + map2);
+        System.out.println("map1 = " + map1);
+        System.out.println();
 
+        // 3. 전체 Key 자료조회
+        System.out.println(" 전체 Key 자료조회 map1.keySet()");
+        Set<String> keySet = map1.keySet();
+        for (String s : keySet) {
+            System.out.println("\tKey 검색 = [" + s + "]");
+        }
+        System.out.println("map1 = " + map1);
+        System.out.println();
+
+        // 4. 전체 Value 자료 조회
+        System.out.println(" 전체 Value 자료조회 map.values()");
+        Collection<String> values = map1.values();
+        for (String s : values) {
+            System.out.println("\tValue 검색[" + s + "]");
+        }
+        System.out.println("map1 = " + map1);
     }
 }
