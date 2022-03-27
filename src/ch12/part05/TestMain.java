0bdfebe573edc0cf6ba5e77d34cdc529c5524c0f
gigkgka <leesw1347@gmail.com>
2022-03-28 00:39:00 +0900

    Properties 기본 예제 [1] - 기본 CRUD

---
+++ src/ch12/part05/TestMain.java
@@ -0,0 +1,9 @@
+package ch12.part05;
+
+public class TestMain {
+    public static void main(String[] args) {
+        // 객체생성 및 자료등록 put()
+        System.out.println(" Properties 객체생성 및 자료등록 ");
+
+    }
+}
