7649c43424d270518b87e9f80b50b9d724a37bd5
gigkgka <leesw1347@gmail.com>
2022-04-04 04:39:21 +0900

    쓰레드 풀 종료 - shutdown(), shutdownNow(), awaitTermination()

--- src/ch14/part05/TestMain.java
+++ src/ch14/part05/TestMain.java
@@ -1,7 +1,9 @@
 package ch14.part05;
 
+import java.text.MessageFormat;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class TestMain {
     public static void main(String[] args) {
@@ -14,8 +16,22 @@ public class TestMain {
         pool.execute(new MyRunnable("thread3", 6));
 
         // 1. shutdown 함수 사용 - 주색헤재해서 사용하세요
-        pool.shutdown();
+//        pool.shutdown(); // 현재 진행되거나 대기 중인 작업은 실행되지만, 새로운 작업은 실행하지 않는다
 
+        // 2. shutdownNow() 함수 사용
+//        pool.shutdownNow(); // 현재 진행 중인 작업에서 InterruptedException을 발생 시키며, 작업은 진행된다 대기 중이거나
+        // 새로운 작업은 실행하지 않는다
+
+        // 3. awaitTermination() 함수의 사용
+        try {
+            boolean awaitTermination =
+                    pool.awaitTermination(3500, TimeUnit.MILLISECONDS);
+            System.out.println(MessageFormat.format("awaitTermination {0}", awaitTermination));
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("쓰레드 종료");
     }
 
     // Runnable 인터페이슥 구현 MyRunnable 클래스 정의
