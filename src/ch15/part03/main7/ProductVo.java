e369c3068a91d01e0167a7a007be0e1138114321
gigkgka <leesw1347@gmail.com>
2022-04-17 17:46:12 +0900

    1. ObjectOutputStream을 이용한 쓰기작업

---
+++ src/ch15/part03/main7/ProductVo.java
@@ -0,0 +1,6 @@
+package ch15.part03.main7;
+
+import java.io.Serializable;
+
+public class ProductVo implements Serializable {
+}
---
+++ src/ch15/part03/main7/TestMain.java
@@ -0,0 +1,7 @@
+package ch15.part03.main7;
+
+public class TestMain {
+    public static void main(String[] args) {
+
+    }
+}
