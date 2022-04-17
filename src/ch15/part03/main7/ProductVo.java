291c0f0ba50af79581a12027df7221d322a75b50
gigkgka <leesw1347@gmail.com>
2022-04-17 17:51:56 +0900

    1. ObjectOutputStream을 이용한 쓰기작업

--- src/ch15/part03/main7/ProductVo.java
+++ src/ch15/part03/main7/ProductVo.java
@@ -2,5 +2,48 @@ package ch15.part03.main7;
 
 import java.io.Serializable;
 
-public class ProductVo implements Serializable {
+public class ProductVo implements Serializable { // 직렬화
+    private String productNo;
+    private String productName;
+    private int price;
+
+    public ProductVo() {
+
+    }
+
+    public ProductVo(String productNo, String productName, int price) {
+        this.productNo = productNo;
+        this.productName = productName;
+        this.price = price;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getProductNo() {
+        return this.productNo;
+    }
+
+    public void setProductNo(String productNo) {
+        this.productNo = productNo;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductVo[productNo=" + productNo + ". productName=" +
+                productName + ". price=" + price + "]";
+    }
 }
