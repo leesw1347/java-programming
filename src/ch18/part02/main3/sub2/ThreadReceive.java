1fb9d37ccf70a4b9d550dac2a1a86dbbb363c8c7
leesw1347 <leesw1347@gmail.com>
2022-04-24 18:01:29 +0900

    * 서버 -> 클라이언트, 클라이언트 -> 서버로 부터 자료를 전송 받기 위한 클래스
    * 수신된 자료를 화면에 쓰기 작업을 하는 클래스는 ClientController 클래스 객체이다
    
    ThreadReceive 클래스는 ClientController 객체를 직접 받는것이 아닌 IClientUser 타입을 만들어서 해당 타입으로 객체를 받는다

--- src/ch18/part02/main3/sub2/ThreadReceive.java
+++ src/ch18/part02/main3/sub2/ThreadReceive.java
@@ -1,4 +1,12 @@
 package ch18.part02.main3.sub2;
 
-public class ThreadReceive {
+import java.net.Socket;
+import java.util.Map;
+
+public class ThreadReceive extends Thread {
+    public ThreadReceive(Socket accept, String server, Map<Socket, Thread> sendThreadMap) {
+    }
+
+    public ThreadReceive(Socket socket, String name, IClientUser clientUser) {
+    }
 }
