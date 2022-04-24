2ec41909df1304a31ea5228fc822274ee95267d4
leesw1347 <leesw1347@gmail.com>
2022-04-24 17:45:29 +0900

    * 클라이언트와 접속 후 통신용 Socket 생성 후 해당 Socket을 이용하여  * 전송용 쓰레드와 수신용 쓰레드를 생성하여 서버와 통신을 함

--- src/ch18/part02/main3/sub2/TcpClient.java
+++ src/ch18/part02/main3/sub2/TcpClient.java
@@ -1,5 +1,6 @@
 package ch18.part02.main3.sub2;
 
+import java.io.IOException;
 import java.net.Socket;
 
 /**
@@ -7,4 +8,58 @@ import java.net.Socket;
  */
 public class TcpClient {
     private Socket socket = null;
+    private ThreadSend st = null;
+    IClientUser clientUser = null;
+    String name = null;
+
+    /**
+     * ClientUI 화면에서 "서버접속" 클릭 시 해당 생성자 함수 호출
+     */
+    public TcpClient(String name, IClientUser clientUser) {
+        this.name = name;
+        this.clientUser = clientUser;
+    }
+
+    /**
+     * 클라이언트 접속 함수
+     */
+    public void connect() {
+        // 클라이언트 소켓 생성
+        try {
+            socket = new Socket("localhost", 12225);
+
+            // 쓰기 작업 쓰레드
+            st = new ThreadSend(socket, name);
+
+            // 읽기 작업 쓰레드
+            ThreadReceive rt = new ThreadReceive(socket, name, clientUser);
+            rt.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 클라이언트 통신종료 함수
+     */
+    public void close() {
+        if (socket != null) {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * UI 객체 TcpClient 쓰기 작업
+     * SendThread 쓰기 작업
+     */
+    public void setMessage(String msg) {
+        System.out.println("st = " + st + " : " + msg);
+        if (st != null) {
+            st.sendMessage(msg);
+        }
+    }
 }
