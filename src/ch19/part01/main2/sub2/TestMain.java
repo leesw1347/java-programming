368f26b4447b6411cea39bc025937a40f1aa79e8
leesw1347 <leesw1347@gmail.com>
2022-04-30 10:25:23 +0900

    * 람다식 사용 예제2

---
+++ src/ch19/part01/main2/sub2/TestMain.java
@@ -0,0 +1,15 @@
+package ch19.part01.main2.sub2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestMain {
+    public static void main(String[] args) {
+        // 리스트생성
+        List<String> names1 = new ArrayList<>();
+        names1.add("b");
+        names1.add("c");
+
+
+    }
+}
