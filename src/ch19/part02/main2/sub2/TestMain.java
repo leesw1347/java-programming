1a79b39d79e67ac98c209b73c33ec02609f95f55
leesw1347 <leesw1347@gmail.com>
2022-04-30 18:58:36 +0900

    * Stream 생성활용 예제

---
+++ src/ch19/part02/main2/sub2/TestMain.java
@@ -0,0 +1,11 @@
+package ch19.part02.main2.sub2;
+
+public class TestMain {
+    public static void main(String[] args) {
+        // 배열선언
+        String[] array = {
+                "a", "b", "c"
+        };
+        System.out.println(array.toString());
+    }
+}
